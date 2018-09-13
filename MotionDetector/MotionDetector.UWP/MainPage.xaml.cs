c8293e82d1912df08f82e281719a4283b8951ff6
Daniel <danek22@op.pl>
2018-09-13 06:44:43 +0200

    MotionDetector code sample commited.

---
+++ MotionDetector/MotionDetector.UWP/MainPage.xaml.cs
@@ -0,0 +1,84 @@
+﻿using MotionDetector.UWP.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
+using Windows.Devices.Gpio;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MotionDetector.UWP
+{
+    public sealed partial class MainPage : Page
+    {
+
+        private GpioController gpio;
+        private GpioPin sensor;
+        private AzureIoTHubService _azureIoTHubService;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.InitializeComponent();
+
+            InitGPIO();
+            InitAzureIoTHub();
+        }
+
+        private void InitAzureIoTHub()
+        {
+            _azureIoTHubService = new AzureIoTHubService();
+        }
+
+        private void InitGPIO()
+        {
+            gpio = GpioController.GetDefault();
+            if (gpio == null)
+                return;
+            GpioStatus.Text = "Gpio initialized";
+
+            sensor = gpio.OpenPin(16);
+            sensor.SetDriveMode(GpioPinDriveMode.Input);
+            sensor.ValueChanged += Sensor_ValueChanged;
+        }
+
+        private async void Sensor_ValueChanged(GpioPin sender, GpioPinValueChangedEventArgs args)
+        {
+            if (args.Edge == GpioPinEdge.RisingEdge)
+            {
+                System.Diagnostics.Debug.WriteLine("MOTION DETECTED");
+
+                await _azureIoTHubService.SendDataToAzure(new Model.MotionEvent());
+
+                await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                 () =>
+                 {
+                     textPlaceHolder.Text = "MOTION DETECTED";
+                 });
+            }
+
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("NO MOTION DETECTED");
+                await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                 () =>
+                 {
+                     textPlaceHolder.Text = "NO MOTION DETECTED";
+                 });
+            }
+        }
+    }
+}
